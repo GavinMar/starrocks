0609c0e05a0b0626200dc89b8ddff4b4b3048603
GavinMar <majinlin@starrocks.com>
2022-08-15 21:05:53 +0800

    [Feature] Add block cache interfaces.

--- be/src/cache/block_cache.h
+++ be/src/cache/block_cache.h
@@ -10,13 +10,7 @@ typedef std::string CacheKey;
 
 class BlockCache {
 public:
-    ~BlockCache() {}
-
-    BlockCache(const BlockCache&)=delete;
-    BlockCache& operator=(const BlockCache&)=delete;
-
-    // Return a singleton block cache instance
-    static BlockCache& get_instance();
+    static BlockCache* instance();
 
     // Set block size as a logical cache unit
     Status set_block_size(size_t block_size);
@@ -24,10 +18,10 @@ public:
     // Set memory space size for memory tier
     Status set_mem_space(size_t mem_size);
 
-    // Set disk space for disk tier, and it can consist of several different directories 
+    // Set disk space for disk tier, and it can consist of several different directories
     Status set_disk_space(const std::vector<std::string>& dirs, const std::vector<size_t>& sizes);
 
-    // Write data to cache, the offset must be aligned by block size 
+    // Write data to cache, the offset must be aligned by block size
     Status write_cache(const CacheKey& cache_key, off_t offset, size_t size, const void* buffer,
                        size_t ttl_seconds = 0);
 
@@ -35,12 +29,10 @@ public:
     // will be returned. The offset must be aligned by block size.
     StatusOr<size_t> read_cache(const CacheKey& cache_key, off_t offset, size_t size, void* buffer);
 
-    // Remove data from cache. The offset must be aligned by block size 
+    // Remove data from cache. The offset must be aligned by block size
     Status remove_cache(const CacheKey& cache_key, off_t offset, size_t size);
 
 private:
-    BlockCache() {}
-
     size_t _block_size = 0;
 };
 
